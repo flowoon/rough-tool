a7da2d7e9f051fe000e4847b18cdb35d835182c0
우성호 <wooncloud@flow.team>
2024-01-29 17:24:17 +0900

    remove console log

--- js/json-parser.js
+++ js/json-parser.js
@@ -4,11 +4,8 @@ const $after = document.getElementById('after');
 const formatAndCopyJSON = () => {
     try {
         const decodedData = decodeURIComponent($before.value);
-        console.log(decodedData);
         const parsedJSON = JSON.parse(decodedData);
-        console.log(parsedJSON);
         const formattedJSON = JSON.stringify(parsedJSON, null, 4);
-        console.log(formattedJSON);
         $after.value = formattedJSON;
     } catch (e) {
         console.error('Error in formatting JSON: ', e);
